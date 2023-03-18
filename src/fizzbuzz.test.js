443da0a42db0ea5a1850eaa7b2e772e8ea79dd26
Elian <eliantito234@gmail.com>
2023-03-18 10:14:22 -0400

    Primer ciclo de vida de FizzBuzz

---
+++ src/fizzbuzz.test.js
@@ -0,0 +1,10 @@
+describe("FizzBuzz", () => {
+    it("Deberia mostrar numero en cadena", () => {
+        const numero = FizzBuzz(3);
+      expect(numero).toEqual(true);
+    });
+  });
+
+function FizzBuzz(numero) {
+    return true;
+}
