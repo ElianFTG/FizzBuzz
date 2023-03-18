8f523177cf0635d2cfc5f1c21857d996346ef6b7
Elian <eliantito234@gmail.com>
2023-03-18 12:14:36 -0400

    Devuelve una secuencia en string de numero y dependiendo a los Multiplos de 3 o 5 Devuelve Fizz o Buzz o FizzBuzz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,14 +1,23 @@
 function fizzBuzz(numero) {
-    const num = numero;
-    var cadena = num.toString();
-    if (num % 3 === 0 && num % 5 === 0){
-        cadena = "FizzBuzz"
-    }
-    else if(num % 3 === 0){
-        cadena = "Fizz";
-    }
-    else if (num % 5 === 0) {
-        cadena = "Buzz";
+    var num;
+    let cadena = "";
+    for (var i = 0; i < numero; i ++) {
+        num = i + 1;
+        if (num % 3 === 0 && num % 5 === 0){
+            cadena += "FizzBuzz"
+        }
+        else if(num % 3 === 0){
+            cadena += "Fizz";
+        }
+        else if (num % 5 === 0) {
+            cadena += "Buzz";
+        }
+        else {
+            cadena += num.toString();
+        }
+        if(i != numero - 1) {
+            cadena += ",";
+        }
     }
     return cadena;
 }
