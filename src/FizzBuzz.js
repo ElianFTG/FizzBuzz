83ff7359f396de911cee5efed4b88176ea842a96
Elian <eliantito234@gmail.com>
2023-03-18 11:23:46 -0400

    Devuelve Fizz en los multiplos de 3

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,6 +1,9 @@
 function fizzBuzz(numero) {
     const num = numero;
     var cadena = num.toString();
+    if (num % 3 === 0) {
+        cadena = "Fizz"
+    }
     return cadena;
 }
 
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -6,8 +6,15 @@ describe("FizzBuzz", () => {
       expect(numero).toEqual('3');
     });*/
     it("Deberia devolver un numero en cadena", () => {
-        const numero = fizzBuzz(3);
-      expect(numero).toEqual('3');
+        const numero = fizzBuzz(4);
+      expect(numero).toEqual('4');
     });
-    
+    it("Deberia devolver un Fizz si es 3", () => {
+        const numero = fizzBuzz(3);
+      expect(numero).toEqual('Fizz');
+    });
+    it("Deberia devolver un Fizz si en los multiplos de 3", () => {
+        const numero = fizzBuzz(6);
+      expect(numero).toEqual('Fizz');
+    });
 });
